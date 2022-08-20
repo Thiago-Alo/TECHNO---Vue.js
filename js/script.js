45dbbb954d00436e491f158e335ed8c628b4cb93
Thiago Alô <thiago_kadri@hotmail.com>
2022-08-20 18:47:55 +0100

    add manual api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const vm = new Vue({
+    el: '#app',
+    data: {
+        produtos: []
+    },
+    methods:{
+        fetchProdutos(){
+            fetch('./api/produtos.json').then(response => response.json())
+            .then(response => {
+                this.produtos = response;
+            })
+        }
+    },
+    created(){
+        this.fetchProdutos()
+    }
+})
