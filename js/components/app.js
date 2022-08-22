51f0bfa3f211b3a7dc9b7ead3a4004c8cc165028
Thiago Alô <thiago_kadri@hotmail.com>
2022-08-22 13:35:58 +0100

    add router, msg success

--- js/components/app.js
+++ js/components/app.js
@@ -4,6 +4,9 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    mensagemAlerta: 'Item Adicionado',
+    alertaAtivo: false,
+    carrinhoAtivo: true,
   },
   filters: {
     // atraves do | numeroPreco, aplica o retorno na varial
@@ -57,12 +60,20 @@ const vm = new Vue({
         this.produto = false;
       }
     },
+    // fecha o modal do carrinho comparando o target do click (MODO DESCONSTRUIDO)
+    clickForaCarrinho({target, currentTarget}){
+      if(target === currentTarget){
+        this.carrinhoAtivo = false;
+      }
+    },
     // Adiciona as informações do item selecionado ao carrinho
     adicionarItem(){
       this.produto.estoque--;
       //Descontroi as informações do produto para ocupar apenas uma variavel 
       const { id, nome, preco } = this.produto;
       this.carrinho.push({ id, nome, preco });
+      //Executa a função alerta( mensagem ) ao adicionar item ao carrinho
+      this.alerta(`${nome} foi adicionado ao carrinho`)
     },
     //Remove o item do carrinho com o metro splicae()
     //O método SPLICE(de que quantidade até que quantidade quer remover)
@@ -75,18 +86,42 @@ const vm = new Vue({
       if(window.localStorage.carrinho){
         this.carrinho = JSON.parse(window.localStorage.carrinho);
       }
+    },
+    //Exibe a msg de alerta por 1.5seg e retorna para false
+    alerta(mensagem){
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() =>{
+        this.alertaAtivo = false;
+      }, 1500)
+    },
+    //Verifica o link do produto ex: http://localhost:3000/#smartwatch
+    //Se existir faz o fetchProduto com o item passado na const HASH e remove o #
+    router() {
+      const hash = document.location.hash;
+      if (hash)
+        this.fetchProduto(hash.replace("#", ""));
     }
   },
   watch:{
     // Salva os dados dentro de carrinho no localStorage como STRING através do JSON.stringify(this.carrinho)
     carrinho(){
       window.localStorage.carrinho = JSON.stringify(this.carrinho);
-    }
+    },
+    //Fica assistindo o link ex: http://localhost:3000/#smartwatch
+    //E guarda o ID do produto na const HASH
+    produto() {
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`);
+    },
   },
   // faz o fetch do JSON produtos ao ser criado, antes do MOUNT
   created() {
     this.fetchProdutos();
     //Verifica se dados em cache no LocalStorage
     this.checarLocalStorage();
+    //faz o fetchProduto antes do MOUNT
+    this.router();
   }
 })
